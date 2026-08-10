ef5ba047074aa6ba0f3e74c40d919132ca377e56
Manideep_Singh <124759841+mani070707@users.noreply.github.com>
2026-08-10 18:34:29 +0530

    Time: 0 ms (100%), Space: 44.1 MB (17.2%) - LeetHub

---
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void preorder(TreeNode node,List<TreeNode> nodes){
+        if(node == null) return ;
+        //root -> left - >right
+        nodes.add(node);
+        preorder(node.left,nodes);
+        preorder(node.right,nodes);
+    }
+    public void flatten(TreeNode root) {
+        if(root == null){
+            return;
+        }
+        List<TreeNode> nodes = new ArrayList<>();
+        preorder(root,nodes);
+
+        for(int i=0;i<nodes.size()-1;i++){
+            nodes.get(i).left = null;
+            nodes.get(i).right = nodes.get(i+1);
+        }
+    }
+}
