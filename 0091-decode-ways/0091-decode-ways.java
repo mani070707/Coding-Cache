936c33def8c9d85fb9744021a137ec771ff4eec4
Manideep_Singh <124759841+mani070707@users.noreply.github.com>
2026-08-18 14:10:05 +0530

    Time: 1 ms (79.92%), Space: 42.8 MB (93.61%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -20,8 +20,26 @@ class Solution {
     }
     public int numDecodings(String s) {
         int n = s.length();
-        int[] dp = new int[n];
-        Arrays.fill(dp, -1);
-        return helper(0,s,dp);
+        int[] dp = new int[n+1];
+        // Arrays.fill(dp, -1);
+        // return helper(0,s,dp);
+
+        dp[n] = 1; // empty suffix - 1way
+
+        for(int idx=n-1;idx>=0;idx--){
+            if(s.charAt(idx) == '0') continue;
+            int one = dp[idx+1];
+            int two = 0;
+            if(idx + 1 < s.length()){
+                int num = (s.charAt(idx)-'0')*10 + (s.charAt(idx+1)-'0');
+                if(num<=26){
+                    two = dp[idx+2];
+                }
+            }
+
+            dp[idx] = one + two;
+        }
+        return dp[0];
+
     }
 }
