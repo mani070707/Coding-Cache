2b4bf1cc111e69b1a0bdf84a7c62ceafb54ae0aa
Manideep_Singh <124759841+mani070707@users.noreply.github.com>
2026-09-11 19:08:10 +0530

    Time: 119 ms (27.02%), Space: 48.4 MB (6.46%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,31 +1,22 @@
 class Solution {
-    public boolean isPalindrome(String s,int i,int j, boolean[][] dp){
-        //base case
-        if(i>=j) return true;
-        //memo
-        if(dp[i][j] != false) return dp[i][j];
-        //logic
-        Boolean res = (s.charAt(i) == s.charAt(j) && isPalindrome(s,i+1,j-1,dp));
-        dp[i][j] = res;
-        return res; 
-    }
     public String longestPalindrome(String s) {
         int n = s.length();
-        boolean[][] dp = new boolean[n][n];
-        
         int st = 0;
         int maxLen = 0;
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(isPalindrome(s,i,j,dp) && (j-i+1) > maxLen){
-                    st = i;
-                    maxLen = j-i+1;
+        boolean dp[][] = new boolean[n][n];
+        
+        for(int i=n-1;i>=0;i--){
+            for(int j=i;j<n;j++){
+                if(s.charAt(i) == s.charAt(j) && (j-i <= 2 || dp[i+1][j-1])){
+                    dp[i][j] = true;
+                    if(maxLen < (j-i+1)){
+                        st = i;
+                        maxLen = j-i+1;
+                    }
                 }
             }
         }
 
         return s.substring(st,st+maxLen);
     }
-    
 }
