f5677650a2d1516bebebb4870caa8e7794337dc7
Manideep_Singh <124759841+mani070707@users.noreply.github.com>
2026-09-14 08:01:17 +0530

    Time: 22 ms (45.82%), Space: 53.8 MB (81.16%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -21,10 +21,31 @@ class Solution {
         int[][] dp = new int[n+1][m+1];
 
         for(int[] row : dp){
-            Arrays.fill(row,-1);
+            Arrays.fill(row,0);
         }
 
-        return helper(n,m,text1,text2,dp);
+        // return helper(n,m,text1,text2,dp);
+
+
+        for(int i=0;i<=n;i++){
+            dp[i][0] = 0;
+        }
+        for(int j=0;j<=m;j++){
+            dp[0][j] = 0;
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(text1.charAt(i-1) == text2.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = 0 + Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+
+        return dp[n][m];
 
     }
 }
