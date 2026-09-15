4b9585f5fc37a022295ff912739cd7152dac3527
Manideep_Singh <124759841+mani070707@users.noreply.github.com>
2026-09-15 07:38:45 +0530

    Time: 6 ms (8.42%), Space: 47.3 MB (35.15%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -24,9 +24,34 @@ class Solution {
         int[][] dp = new int[n+1][m+1];
 
         for(int[] row : dp){
-            Arrays.fill(row,-1);
+            Arrays.fill(row,0);
         }
 
-        return helper(n,m,word1,word2,dp);
+        // return helper(n,m,word1,word2,dp);
+
+        //base
+
+        for(int i=0;i<=n;i++){
+            dp[i][0] = i;
+        }
+        for(int j=0;j<=m;j++){
+            dp[0][j] = j;
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(word1.charAt(i-1) == word2.charAt(j-1)){
+                    dp[i][j] = 0 + dp[i-1][j-1];
+                }
+                else{
+                    int insert = dp[i][j-1];
+                    int delete = dp[i-1][j];
+                    int replace = dp[i-1][j-1];
+
+                    dp[i][j] = 1 + Math.min(delete,Math.min(insert,replace));
+                }
+            }
+        }
+        return dp[n][m];
     }
 }
